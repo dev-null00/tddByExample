3650a6e84c7effaa226ebb6ba06fa0065804e710
Solomon Lasluisa <solomon@club-os.com>
2017-01-07 14:43:31 -0500

    remove duplicate equals in objects

--- src/main/java/com/example/Dollar.java
+++ src/main/java/com/example/Dollar.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.Objects;
-
 class Dollar extends Money{
     Dollar(final int amount, final String currency) {
         super(amount, currency);
@@ -13,18 +11,5 @@ class Dollar extends Money{
 
     String currency() {
         return currency;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Money money = (Money) o;
-        return amount == money.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
--- src/main/java/com/example/Franc.java
+++ src/main/java/com/example/Franc.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.Objects;
-
 class Franc extends Money{
     Franc(final int amount, final String currency) {
         super(amount, currency);
@@ -13,17 +11,5 @@ class Franc extends Money{
 
     String currency() {
         return currency;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        final Money money = (Money) o;
-        return amount == money.amount && currency == money.currency;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
