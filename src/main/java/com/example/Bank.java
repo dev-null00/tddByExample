8450898bdf6b751b79ca806bc83d5d51ee8383fd
Solomon Lasluisa <solomon@club-os.com>
2017-01-07 15:13:49 -0500

    refactor our the old hardcoded values

--- src/main/java/com/example/Bank.java
+++ src/main/java/com/example/Bank.java
@@ -1,7 +1,9 @@
 package com.example;
 
 class Bank {
-    Money reduce(final Expression sum, final String currency) {
-        return new Money(15, currency);
+    Money reduce(final Expression expression, final String currency) {
+        final Sum sum = (Sum) expression;
+        final int total = sum.money1.amount + sum.money2.amount;
+        return new Money(total, currency);
     }
 }
