929dc08c8bab91a05a532f85bed0fcf6db2daf9a
Solomon Lasluisa <solomon@club-os.com>
2017-01-07 11:23:31 -0500

    set out reminders

---
+++ src/test/java/com/example/CurrencyAdditionTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrencyAdditionTest {
+    /*
+    1. quickly add a test
+    2. Run all tests and see the new on fail
+    3. make a little change
+    4. run all test and see them all succeed
+    5. refactor to remove duplication
+     */
+    @Test
+    public void nothing() throws Exception {
+
+    }
+
+}
